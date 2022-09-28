016b00f9516ee49cb06706a362d789aba300c8c3
Thomas Müller <1005065+DeepDiver1975@users.noreply.github.com>
2022-09-28 15:55:18 +0200

    feat: add German translation

---
+++ resources/lang/de/validation.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    /*
+      |--------------------------------------------------------------------------
+      | Validation Language Lines
+      |--------------------------------------------------------------------------
+     */
+
+    'credit_card_rule_failed' => 'Ungültige Kredit- oder Debitkarte.',
+    'credit_card_rule_with_security_code_failed' => 'Ungültige Kredit- oder Debitkarte oder Sicherheitscode stimmt nicht mit dem erwarteten Format überein',
+];
