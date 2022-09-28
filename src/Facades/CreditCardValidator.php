122e18bfe566646ee389c0ebe846b11ced80d0a4
Thomas Müller <1005065+DeepDiver1975@users.noreply.github.com>
2022-09-28 16:05:29 +0200

    feat: add PHPDoc method annotations to facade

--- src/Facades/CreditCardValidator.php
+++ src/Facades/CreditCardValidator.php
@@ -20,7 +20,27 @@
 namespace Jlorente\Laravel\CreditCards\Facades;
 
 use Illuminate\Support\Facades\Facade;
+use Jlorente\CreditCards\CreditCardTypeConfig;
 
+/**
+ * @method static CreditCardTypeConfig|null getType(string $cardNumber)
+ * @method static bool isValid(string $cardNumber)
+ * @method static bool is(string $cardType, string $cardNumber)
+ * @method static array getAllowedTypesList()
+ *
+ * @method static bool isVisa(string $cardNumber)
+ * @method static bool isMastercard(string $cardNumber)
+ * @method static bool isAmericanExpress(string $cardNumber)
+ * @method static bool isDinersClub(string $cardNumber)
+ * @method static bool isDiscover(string $cardNumber)
+ * @method static bool isJCB(string $cardNumber)
+ * @method static bool isUnionPay(string $cardNumber)
+ * @method static bool isMaestro(string $cardNumber)
+ * @method static bool isElo(string $cardNumber)
+ * @method static bool isMir(string $cardNumber)
+ * @method static bool isHiper(string $cardNumber)
+ * @method static bool isHiperCard(string $cardNumber)
+ */
 class CreditCardValidator extends Facade
 {
 
